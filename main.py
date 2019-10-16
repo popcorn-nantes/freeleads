6307395b9d03ca9e5e100d1cd956bc1e3c850d67
Jocelyn Griselle <dev@griselle.io>
2019-10-16 18:18:57 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,100 @@
+from twython import TwythonStreamer
+from pymongo import MongoClient
+import os
+import slack
+from vocabulary import BANS, TECHS, RH
+
+
+client = MongoClient()
+db = client.freeleads
+tweets = db.tweet
+leads = db.lead
+
+
+SLACK_API_TOKEN = os.environ.get('SLACK_API_TOKEN')
+TWITTER_APP_KEY = os.environ.get('SLACK_API_TOKEN')
+TWITTER_APP_SECRET = os.environ.get('SLACK_API_TOKEN')
+TWITTER_OAUTH_TOKEN = os.environ.get('SLACK_API_TOKEN')
+TWITTER_OAUTH_TOKEN_SECRET = os.environ.get('SLACK_API_TOKEN')
+
+
+class BColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class LeadStreamer(TwythonStreamer):
+
+    def on_success(self, data):
+        if data.get("retweeted_status"): # filter retweets
+            print(BColors.WARNING + "RETWEET" + BColors.ENDC)
+            return
+        self.detect_lead(data)
+
+    def on_error(self, status_code, data):
+        print(status_code)
+        print(data)
+        self.disconnect()
+
+    def detect_lead(self, data):        
+        text, hashtags = self.get_text(data)
+        url = self.get_url(data)
+        text_lowered = text.lower()   
+
+        for ban in BANS:
+            if ban in text_lowered or ban in hashtags:
+                return
+
+        for tech in TECHS:
+            if tech in text_lowered or tech in hashtags:
+                tweet_id = tweets.insert_one(data).inserted_id
+                print(BColors.OKBLUE + "TECH" + BColors.ENDC)
+                for rhword in RH:
+                    if rhword in text_lowered or rhword in hashtags:
+                        lead_id = leads.insert_one(data).inserted_id
+                        print(BColors.OKGREEN + "LEAD" + BColors.ENDC)
+                        self.post(url)
+                        break
+                break
+
+    def post(self, url):
+        response = slackclient.chat_postMessage(
+            as_user=False,
+            username="Twitter bot",
+            channel='#tuyaux-boulot-twitter-bot',
+            text=url
+        )
+
+    def get_text(self, data):
+        text = data.get("extended_tweet", {}).get("full_text") or data.get("text")
+        print(BColors.HEADER + text + BColors.ENDC)
+
+        hashtags = data.get("entities", {}).get("hashtags", [])
+        hashtags = [h["text"].lower() for h in hashtags]
+        print(hashtags)
+
+        return text, hashtags
+
+    def get_url(self, data):
+        tweet_id = data.get("id")
+        user = data.get("user")
+        user_name = user and user.get("screen_name")
+
+        if user_name and tweet_id:
+            url = f"https://twitter.com/{user_name}/status/{tweet_id}"
+        elif data.get("entities", {}).get("urls"):
+            url = data.get("entities", {})["urls"][0].get("url")
+        else:
+            url = ''
+        return url
+
+
+slackclient = slack.WebClient(token=SLACK_API_TOKEN, ssl=False)
+stream = LeadStreamer(TWITTER_APP_KEY, TWITTER_APP_SECRET, TWITTER_OAUTH_TOKEN, TWITTER_OAUTH_TOKEN_SECRET)
+stream.statuses.filter(track=TECHS, language="fr")
---
+++ vocabulary.py
@@ -0,0 +1,51 @@
+BANS = ["émission", "emission", "émissions", "emissions", 
+        "soumission", "transmission", "soumissions", "transmissions",
+        "commission", "commissions",
+        "permission", "permissions",
+        "admission", "admissions",
+        "compromissions", "compromission"
+]
+
+TECHS = [
+    "python", "django", "flask", "scrapy", "scikit",
+    "ruby", "rails", #"sinatra",
+    "javascript", "vuejs", "nodejs", "reactjs", "angularjs", "emberjs", "vue-js", "node-js", "react-js", "angular-js", "ember-js",
+    "typescript", "type-script",
+    "C++",
+    "docker", "kubernetes", "k8",
+    "java", "spring",
+    "scala", "hadoop",
+    "php", "symphony", "laravel", "wordpress",
+    "css", 
+    "golang", #"hugo",
+    "kotlin", "android"
+    "swift", "ios",
+    #".net, ,
+    #"c#, ,
+]
+
+QUALIFY = [
+    "developpeur", "développeur", "developpeurs", "développeurs",
+    "dev", "devs", "devop", "devops", 
+    "ingénieur", "ingenieur", "ingénieurs", "ingenieurs"
+    "fullstack", "fullstacks", "stack", "full-stack", "full-stacks"
+]
+
+RH = [
+    #"recrute", "recrutent", "recrutes"
+    #"recrutement", "recrutements",
+    #"besoin", "besoins",
+    "mission", "missions", 
+    "freelance", "freelances", "free-lance", "free-lances",
+    "frees-lances",
+    "free lance", "free lances", "frees lances",
+    #"cherche", "cherchent",
+    #"recherche", "recherchent",
+    #"offre", "offres", "offrent"
+    #"job", "jobs", 
+    #"bosser", "bossessera", "bosses"
+]
+
+LOCALISATION = [
+    "nantes", "44", "loire-atlantique", "remote", "à distance", "teletravail", "télétravail", "tele-travail", "télé-travail"
+]
