cb509e304d65718b9068515037a20d9fdb30d825
Jocelyn Griselle <dev@griselle.io>
2020-05-08 20:49:28 +0200

    fix on error

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class LeadStreamer(TwythonStreamer):
             return
         self.detect_lead(data)
 
-    def on_error(self, status_code, data):
+    def on_error(self, status_code, data, headers):
         print(status_code)
         print(data)
         self.disconnect()
